9d1e0c506ddc58b4a63d22612479021fe1b4c24d
Angel75Git <aduboncenteno@usf.edu>
2025-02-15 18:25:13 -0500

    Task 4 - Product Shipping Cost Calculation

--- cc_7.js
+++ cc_7.js
@@ -33,3 +33,19 @@ const calculateLoyaltyDiscount = (amount, years) => {
 //Test Cases
 console.log(calculateLoyaltyDiscount(100, 6));
 console.log(calculateLoyaltyDiscount(200, 2));
+
+//Task 4
+//Product Shipping 
+function calculateShippingCost(weight, location, expedited = false){
+    let shipFees = {
+        "USA": (5 + (weight*0.5)),        //Math operations
+        "Canada": (10 +(weight*0.7))}
+
+    if (expedited){
+        return shipFees[location] + 10;    //Test if Expedited
+    } else {
+        return shipFees[location]; 
+    }
+}//Test Data
+console.log(`Shipping Cost: $${calculateShippingCost(10, "USA", true).toFixed(2)}`);
+console.log(`Shipping Cost: $${calculateShippingCost(5, "Canada", false).toFixed(2)}`);
