13f893dbcb2302806b690b8996281b47f685bec5
Angel75Git <aduboncenteno@usf.edu>
2025-02-15 17:29:42 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -2,6 +2,17 @@
 
 function calculateInvoice(subtotal, taxRate, discount){
     return ((subtotal) + (subtotal * taxRate) - discount)
-} // Using template literals around the toFixed method
+}; // Using template literals around the toFixed method
 console.log(`Total Invoice: $${calculateInvoice(100, 0.08, 5).toFixed(2)}`);
 console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20).toFixed(2)}`);
+
+//Task 2
+//Function expression starting with const variable
+const calculateHourlyWage = function(salary, hoursPerWeek){
+    let hourWage = salary / (hoursPerWeek * 52);
+    console.log(`Hourly Wage: $${hourWage.toFixed(2)}`);
+}; 
+//Using a variable to store wage
+
+console.log(calculateHourlyWage(52000, 40)); //Test Data
+console.log(calculateHourlyWage(75000, 35));
